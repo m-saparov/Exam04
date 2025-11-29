40cd4b8266deb87ad6cdc2957b2b87745f50ada2
m-saparov <developer.saparov@gmail.com>
2025-11-29 10:58:26 +0500

    book service added

--- library/services.py
+++ library/services.py
@@ -21,7 +21,7 @@ def create_author(name: str, bio: str = None) -> Author:
 def get_author_by_id(author_id: int) -> Author | None:
     """ID bo'yicha muallifni olish"""
     with get_db() as session:
-        author = session.query(Author).get(id)
+        author = session.query(Author).get(author_id)
     
     return author
 
@@ -39,7 +39,7 @@ def update_author(author_id: int, name: str = None, bio: str = None) -> Author |
     if author:
         with get_db() as session:
             author.name = name if name else author.name
-            author.bio = bio if name else author.bio
+            author.bio = bio if bio else author.bio
 
             session.add(author)
             session.commit()
@@ -56,6 +56,56 @@ def delete_author(author_id: int) -> bool:
             session.delete(author)
             session.commit()
         
+        result = True
+
+    return result
+
+def create_book(title: str, author_id: int, published_year: int, isbn: str = None) -> Book:
+    """Yangi kitob yaratish"""
+    book = Book(
+        title = title,
+        author_id = author_id,
+        published_year = published_year,
+        isbn = isbn
+    )
+
+    with get_db() as session:
+        session.add(book)
+        session.commit()
+
+    return book
+
+def get_book_by_id(book_id: int) -> Book | None:
+    """ID bo'yicha kitobni olish"""
+    with get_db() as session:
+        book = session.query(Book).get(book_id)
+
+    return book
+
+def get_all_books() -> list[Book]:
+    """Barcha kitoblar ro'yxatini olish"""
+    with get_db() as session:
+        books = session.query(Book).all()
+    
+    return books
+
+def search_books_by_title(title: str) -> list[Book]:
+    """Kitoblarni sarlavha bo'yicha qidirish (partial match)"""
+    with get_db() as session:
+        books = session.query(Book).filter(
+            Book.title.ilike(f"%{title}%")
+        ).all()
+    return books
+
+def delete_book(book_id: int) -> bool:
+    """Kitobni o'chirish"""
+    book = get_book_by_id(book_id)
+    result = False
+
+    if book:
+        with get_db() as session:
+            session.delete(book)
+            session.commit()
         result = True
 
     return result
