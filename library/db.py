e7bca7b823233703b248505bc37b463953b7019a
m-saparov <developer.saparov@gmail.com>
2025-11-29 09:06:11 +0500

    database added

---
+++ library/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import (
+    create_engine, URL, MetaData,
+)
+import config
+
+DATABASE_URL = URL.create(
+    "postgresql+psycopg2",
+    username=config.DB_USER,
+    password=config.DB_PASS,
+    host=config.DB_HOST,
+    port=config.DB_PORT,
+    database=config.DB_NAME,
+)
+engine = create_engine(DATABASE_URL)
+metadata_obj = MetaData()
